5842b523e4fdac855b3ce478c0c7424a60b2175d
Geoffrey <geoffrey.vanwiele@gmail.com>
2021-02-22 20:04:16 +0100

    Update and rename Xiaomi-Mop-Pro.sh to Xiaomi-Mop-Pro/influx.sh

--- Xiaomi-Mop-Pro/influx.sh
+++ Xiaomi-Mop-Pro/influx.sh
@@ -24,29 +24,32 @@ Viomi_Mop_Y=$( cat /tmp/${viomi_name}.status | grep "Mop route pattern:" | awk '
 Viomi_Sec_Cleanup=$( cat /tmp/${viomi_name}.status | grep "Secondary Cleanup:" | awk '{print $3}' )
 Viomi_Sound_Vol=$( cat /tmp/${viomi_name}.status | grep "Sound Volume:" | awk '{print $3}' )
 Viomi_Clean_Time=$( cat /tmp/${viomi_name}.status | grep "Clean time:" | awk '{print $3}' )
-Viomi_Clean_Area=$( cat /tmp/${viomi_name}.status | grep "Clean area:" | awk '{print $3" "$4}' )
+Viomi_Clean_Area=$( cat /tmp/${viomi_name}.status | grep "Clean area:" | awk '{print $3}' )
 Viomi_Current_Map=$( cat /tmp/${viomi_name}.status | grep "Current map ID:" | awk '{print $4}' )
 Viomi_Remember_Map=$( cat /tmp/${viomi_name}.status | grep "Remember map:" | awk '{print $3}' )
 Viomi_Has_Map=$( cat /tmp/${viomi_name}.status | grep "Has map:" | awk '{print $3}' )
 Viomi_New_Map=$( cat /tmp/${viomi_name}.status | grep "Has new map:" | awk '{print $4}' )
 Viomi_Amount_Maps=$( cat /tmp/${viomi_name}.status | grep "Number of maps:" | awk '{print $4}' )
 Viomi_Light_State=$( cat /tmp/${viomi_name}.status | grep "Light state:" | awk '{print $3}')
+timestamp=$( date +%s%N)
+# Convert Secondary Cleaning from 0 to False or 1 to True
+if [ "$Viomi_Sec_Cleanup" = "0" ]; then
+                Viomi_Sec_Clean="False"
+        else
+                Viomi_Sec_Clean="True"
+fi
 
-# Telgram Send All Found Settings
-#/usr/bin/telegram "Status for ${viomi_name}"
-#/usr/bin/telegram "Working? ${Viomi_Working_Status}"
-#/usr/bin/telegram "Charging: ${Viomi_Charging}"
-#/usr/bin/telegram "Battery Percentage: ${Viomi_Battery}"
-#/usr/bin/telegram "Box Currently Installed: ${Viomi_Box_Type}"
-#/usr/bin/telegram "Profile Currently Selected: ${Viomi_Mode} with FAN Profile: ${Viomi_Fan_Speed}"
-#/usr/bin/telegram "Vacuum Edges?: ${Viomi_Vacuum_Edges}"
-#/usr/bin/telegram "Water Pump Profile: ${Viomi_Water_Grade}"
-#/usr/bin/telegram "Mop in Y pattern?: ${Viomi_Mop_Y}"
-#/usr/bin/telegram "Secondary Cleanup?: ${Viomi_Sec_Cleanup}"
-#/usr/bin/telegram "Sound Voume: ${Viomi_Sound_Vol}"
-#/usr/bin/telegram "Cleaning Time: ${Viomi_Clean_Time} for area of ${Viomi_Clean_Area}"
-#/usr/bin/telegram "Current Map ID=${Viomi_Current_Map}"
-#/usr/bin/telegram "Has Map?: ${Viomi_Has_Map}"
-#/usr/bin/telegram "Will Remember New Maps? ${Viomi_New_Map}"
-#/usr/bin/telegram "Amount of Known Maps: ${Viomi_Amount_Maps}"
-#/usr/bin/telegram "Light state?: ${Viomi_Light_State}"
+# Write it to a tmp file
+echo "vacuum name=\"$viomi_name\",status=\"$Viomi_Working_Status\",charging=\"$Viomi_Charging\",battery=\"$Viomi_Battery\",boxtype=\"$Viomi_Box_Type\",profile=\"$Viomi_Mode\",fanspeed=\"$Viomi_Fan_Speed\",edges=\"$Viomi_Vacuum_Edges\",waterpump=\"$Viomi_Water_Grade\",ypattern=\"$Viomi_Mop_Y\",seccleanup=\"$Viomi_Sec_Clean\",volume=\"$Viomi_Sound_Vol\",cleantime=\"$Viomi_Clean_Time\",cleanarea=\"$Viomi_Clean_Area\",mapid=\"$Viomi_Current_Map\",hasmap=\"$Viomi_Has_Map\",newmaps=\"$Viomi_New_Map\",knownmaps=\"$Viomi_Amount_Maps\",lightstate=\"$Viomi_Light_State\" $timestamp">/tmp/curl_data.txt
+
+# Send the file contents to to Influxdb
+curl -XPOST "http://$influxdb_host:$influxdb_port/api/v2/write?bucket=$influxdb_db" -d "$(cat /tmp/curl_data.txt)"
+
+# Cleanup
+if [ -f "/tmp/curl_data.txt" ]; then
+        rm -f /tmp/curl_data.txt
+fi
+
+if [ -f "/tmp/${viomi_name}.status" ]; then
+        rm -f /tmp/${viomi_name}.status
+fi

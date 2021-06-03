55b78fffa4544efe5903335977994a3c5a03930b
Geoffrey <geoffrey.vanwiele@gmail.com>
2021-06-03 21:07:55 +0200

    Update backup-pfsense.sh

--- proxmox/backup-pfsense.sh
+++ proxmox/backup-pfsense.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Made to backup pfSense from Proxmox to a TrueNAS Cifs Share
-
 qmid=999
 date=$(date +%Y_%m_%d)
-bntruenas=/mnt/pve/S2S-Backups
+ip=
+nasfolder=$(mount | grep ${ip} | awk '{print $3}' )
+nasbackupfolder=dump
 backupfolder=/var/lib/vz/dump
 backupmode=snapshot
 backupcompression=zstd
@@ -11,18 +12,38 @@ keepbackups=1
 tmpfile=/tmp/backup.log
 
 # Start Backup
-/usr/bin/vzdump ${qmid} --dumpdir ${backupfolder} --mode ${backupmode} --prune-backups keep-last=${keepbackups}  --compress ${backupcompression} >${tmpfile}
+#/usr/bin/vzdump ${qmid} --dumpdir ${backupfolder} --mode ${backupmode} --prune-backups keep-last=${keepbackups}  --compress ${backupcompression} >${tmpfile}
 
-# Get File Backup to Variable
-backupped=$( cat ${tmpfile} | grep "creating vzdump archive" | awk '{print $5}' | sed "s/'//g")
-if [ ! -z ${backupped+x} ]; then rm -rf ${tmpfile}; fi
+# Check if VPN Site to Site is UP
+# Ping the NAS and discard output
+ping -c 1 $ip > /dev/null 2> /dev/null
 
-# Copy Backup Files to SMB Share
-if [ -f ${backupfolder}/${backupped##*/} ]; then
-        cp ${backupfolder}/${backupped##*/} ${bntruenas}/site2site/
-fi
+# Check the Exit Code
+if [ $? -eq 0 ]; then
+        # Check if the folder exists and is mounted
+        if [[ -d ${nasfolder}/${nasbackupfolder} ]]; then
+                        # Get File Backup to Variable
+                        backupped=$( cat ${tmpfile} | grep "creating vzdump archive" | awk '{print $5}' | sed "s/'//g")
+                        backupfile=$( echo "${backupped//+(*\/|.*)}" )
 
-# Check if file is the same size
-if [[ $(stat -c%s ${backupfolder}/${backupped##*/}) -eq $(stat -c%s $bntruenas/site2site/${backupped##*/}) ]]; then
-        rm -rf ${backupfolder}/${backupped##*/}
+                        # Check if Backupped Variable is populated
+                        if [ ! -z ${backupped+x} ]; then
+                                rm -rf ${tmpfile}
+                        fi
+
+                        # Copy Backup Files to SMB Share
+                        if [ -f ${backupfolder}/${backupped##*/} ]; then
+                                cp ${backupfolder}/${backupped##*/} ${nasfolder}/${nasbackupfolder}/
+                                cp ${backupfolder}/${backupfile}.log ${nasfolder}/${nasbackupfolder}/
+                        fi
+
+                        # Check if file is the same size
+                        if [[ $(stat -c%s ${backupfolder}/${backupped##*/}) -eq $(stat -c%s ${nasfolder}/${nasbackupfolder}/${backupped##*/}) ]]; then
+                                rm -rf ${backupfolder}/${backupfile}.*
+                        fi
+                else
+                        echo "There is no cifs share mounted from ${ip}"
+                fi
+        else
+                echo "${ip} is unreachable"
 fi
